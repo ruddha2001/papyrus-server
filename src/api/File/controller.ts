95b79ed08e5cd00954df50fb752027a96a55358f
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-01-23 16:34:11 +0530

    :sparkles: Add central error handler

--- src/api/File/controller.ts
+++ src/api/File/controller.ts
@@ -1,1 +1,5 @@
-export const uploadFileController = async (filename: string, buffer: Buffer, mimeType: string, size: number) => {};
+import LoggerInstance from '../../loaders/logger';
+
+export const uploadFileController = async (filename: string, buffer: Buffer, mimeType: string, size: number) => {
+  LoggerInstance.info('File received');
+};
