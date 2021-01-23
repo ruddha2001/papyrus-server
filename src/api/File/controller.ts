067915f3a23e4537993bd3f80dddaaad4a8d67df
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-01-23 19:12:59 +0530

    :sparkles: Add object  details to db

--- src/api/File/controller.ts
+++ src/api/File/controller.ts
@@ -1,7 +1,9 @@
+import database from '../../loaders/database';
 import LoggerInstance from '../../loaders/logger';
 import { uploadFileToS3 } from '../Shared/Services/s3Service';
 
 export const uploadFileController = async (
+  email: string,
   title: string,
   filename: string,
   buffer: Buffer,
@@ -10,6 +12,15 @@ export const uploadFileController = async (
 ) => {
   try {
     const uniquekey = await uploadFileToS3(filename, buffer, mimeType);
+    let { storageObject } = await (await database()).collection('userdata').findOne({ email: email });
+    if (storageObject === undefined) storageObject = {};
+    storageObject[title] = uniquekey;
+    if (Object.keys(storageObject).length === 1)
+      await (await database()).collection('userdata').insertOne({ email: email, storageObject: storageObject });
+    else
+      await (await database())
+        .collection('userdata')
+        .updateOne({ email: email }, { $set: { storageObject: storageObject } });
   } catch (error) {
     LoggerInstance.error(error);
     throw error;
